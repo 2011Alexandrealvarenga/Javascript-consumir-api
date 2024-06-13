868e1e4e2dacf4a51c1817c81023b77c2896db5e
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-06-13 11:51:59 -0300

    carrega os posts

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function loadPosts(){
+  document.getElementById('posts').innerHTML = 'Carregando ...'
+
   fetch('https://jsonplaceholder.typicode.com/posts')
   .then(function(resultado){
     return resultado.json();
@@ -8,15 +10,25 @@ function loadPosts(){
     // retorna em json
     // console.log(json);
     */
-
-    /*
+    
     // exibe a quantidade de posts
-    document.getElementById('posts').innerHTML = json.length + 'posts';
-    */
+    // document.getElementById('posts').innerHTML = json.length + 'posts';
+
+    montarBlog(json);
    
   })
   .catch(function(error){
     console.log('Ocorreu problema');
   });
 
+}
+function montarBlog(lista){
+  let html = '';
+
+  for (let i=0; i<lista.length; i++){
+    html += '<h3>'+lista[i].title+'</h3>';
+    html += lista[i].body+'<br>';
+    html += '<hr>';
+  }
+  document.getElementById('posts').innerHTML = html;
 }
