4d49d6af8b90e5467edecab535a49542d8ac576c
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-06-13 11:35:38 -0300

    exibe a quantidade de posts

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ function loadPosts(){
   .then(function(resultado){
     return resultado.json();
   })
-  // retorna em json
   .then(function(json){
-    console.log(json);
+    /*
+    // retorna em json
+    // console.log(json);
+    */
+
+    /*
+    // exibe a quantidade de posts
+    document.getElementById('posts').innerHTML = json.length + 'posts';
+    */
+   
   })
   .catch(function(error){
     console.log('Ocorreu problema');
